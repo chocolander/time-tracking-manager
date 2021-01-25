2bc02671c887a2ec02c3b163fcda495459014cd3
Loran-l <46041332+Loran-l@users.noreply.github.com>
2021-01-24 20:44:54 -0500

    classes in progress

---
+++ src/components/classes/Task.js
@@ -0,0 +1,10 @@
+class Task {
+    constructor(tName, completeness, activeStatus,
+         categoriesList, timeToCompletem, 
+         assosiatedTimeblocks, recommendedSessionTime){
+
+
+    }
+
+    getName(){return this.tName};
+}
