a478149b0d61970976b1f892533be19574f2d679
Jung Soo Choi <chlwjdtn15@hotmail.com>
2021-01-28 16:23:10 +0900

    Testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react'; 
 import './App.css';
+import { Button } from './components/classesLogginButtons/Button';
+import { HealthBtn } from './components/classesLogginButtons/HealthBtn';
+import {WorkBtn} from './components/classesLogginButtons/WorkBtn'
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <WorkBtn>Work</WorkBtn>
+    <HealthBtn class="healthbtn">Health</HealthBtn>
+    <Button>Hobby</Button>
+    <Button>Family</Button>
+    <Button>Routine</Button>
+    <Button>Friends</Button>
+    <Button>Education</Button>
+    <Button>Entertainment</Button>
+    
+
+
+
     </div>
+
   );
 }
 
