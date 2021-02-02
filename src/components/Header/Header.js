c8710fd95496f7dee069452ed1812cffb5a7f89a
HyunSeongLee <gustjd2680@gmail.com>
2021-02-02 11:02:35 +0900

    Header&Footer version2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'; 
 import './Header.css'; 
+import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
 
 class Header extends Component {
     render() {
         return (
-            <div className="nav">
-                <i class="fas fa-calendar-week"></i>
-                <a href="#home" className="active">Home</a>
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
-            </div>
-
-            
-        )
+          <div className="App">
+    <Navbar bg="light" variant="light">
+          <Navbar.Brand href="#home">
+        Chocolander
+        <i class="fas fa-calendar-week"></i>
+        </Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-primary">Search</Button>
+    </Form>
+  </Navbar>
+          </div>
+        );
     }
 }
 
