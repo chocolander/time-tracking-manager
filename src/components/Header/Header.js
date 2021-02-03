307fcda2718f82910d0c16360142bb5890fdff20
HyunSeongLee <gustjd2680@gmail.com>
2021-02-03 10:23:26 +0900

    Header&Footer version3

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,17 +9,14 @@ class Header extends Component {
     <Navbar bg="light" variant="light">
           <Navbar.Brand href="#home">
         Chocolander
-        <i class="fas fa-calendar-week"></i>
+      <i class="fas fa-calendar-week"></i>
         </Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="#home">Home</Nav.Link>
       <Nav.Link href="#features">Features</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
     </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-primary">Search</Button>
-    </Form>
+ 
   </Navbar>
           </div>
         );
