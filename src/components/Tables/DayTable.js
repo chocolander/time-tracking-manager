1e65d0312c000e8562c7e0d816d13c9103647983
HyunSeongLee <gustjd2680@gmail.com>
2021-02-15 09:00:09 +0900

    Table version 3

--- src/components/Tables/DayTable.js
+++ src/components/Tables/DayTable.js
@@ -1,11 +1,46 @@
 import React, {Component} from 'react'; 
 import './DayTable.css'
+import styled from "styled-components"
+
+const theme = {
+    blue: {
+        default: '#3f51b5',
+        hover: '#283593'
+    },
+    pink: {
+        default: '#e91e63',
+        hover: '#ad1457'
+    }
+}
+
+const Button = styled.button`
+    background-color: ${props => theme[props.theme].default};
+    color: white; 
+    padding: 5px 15px; 
+    border-radius: 5px; 
+    outline: 0;
+    cursor: pointer;
+    box-shadow: 0px 2px 2px lightgray; 
+    transition: ease background-color 250ms;    
+    &:hover {
+        background-color: ${props => theme[props.theme].hover};
+    }
+`
+
+Button.defaultProps = {
+    theme: 'blue'
+}
+
+function clickMe() {
+    alert('Add new schedule functionality!');
+}
 
 class DayTable extends Component {
     render() {
         return (
             <div className="filter">
                 <h2>Daily Schedule</h2>
+                <Button theme="pink" onClick={clickMe}>Add Task +</Button>            
                 <table>
                     <tr>
                         <th>#</th>
